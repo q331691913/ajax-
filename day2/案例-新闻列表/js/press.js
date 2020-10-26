d5068dcc417b70432a67403993fdfff09630c9fd
浅吟 <7824774+shallow_chant@user.noreply.gitee.com>
2020-10-26 21:27:20 +0800

    修改次

--- day2/案例-新闻列表/js/press.js
+++ day2/案例-新闻列表/js/press.js
@@ -1,9 +1,20 @@
 $(function() {
 
+    template.defaults.imports.formatDate = function(data) {
+        let date = new Date(data)
+        let y = date.getFullYear()
+        let m = (date.getMonth() + 1).toString().padStart(2, 0)
+        let d = date.getDate().toString().padStart(2, 0)
+        let h = date.getHours().toString().padStart(2, 0)
+        let miu = date.getMinutes().toString().padStart(2, 0)
+        let s = date.getSeconds().toString().padStart(2, 0)
+        return `${y}-${m}-${d} ${h}:${miu}:${s}`
+    }
     $.ajax({
         type: 'GET',
         url: 'http://www.liulongbin.top:3006/api/news',
         success(res) {
+
             if (res.status !== 200) return alert('没有获取到新闻')
             res.data.forEach(item => item.tags = item.tags.split(','))
             console.log(res);
